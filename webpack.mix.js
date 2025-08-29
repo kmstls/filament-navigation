9a6dc7f5f104730a97f2e1a73dadfab003737bb9
Matthew Myers <mxm1070@users.noreply.github.com>
2025-08-29 18:13:24 -0400

    Upgrade Tailwind.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,3 @@
 const mix = require('laravel-mix')
 
-mix
-    .postCss('resources/css/plugin.css', 'resources/dist/plugin.css', [
-        require('tailwindcss')
-    ])
-    .js('resources/js/plugin.js', 'resources/dist/plugin.js');
+mix.js('resources/js/plugin.js', 'resources/dist/plugin.js');
